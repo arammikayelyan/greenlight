6ce7a9eae8bab543fa79f631936d451918adb442
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-04 22:39:56 +0400

    fixed-format json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,13 @@ import (
 
 // Handler for writing application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Create a fixed-format JSON response from a string.
+	json := `{"status": "available", "environment": %q, "version": %q}`
+	json = fmt.Sprintf(json, app.config.env, version)
+
+	// Set the "Content-Type: application/json"
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON as the HTTP response body.
+	w.Write([]byte(json))
 }
