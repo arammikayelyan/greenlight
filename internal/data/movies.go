1a4d3fef67bc0497bca931788dc379a2dd3d8c35
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-04 23:23:30 +0400

    encoding structs

---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`                // Unique ID integer for the movie
+	CreatedAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
+	Title     string    `json:"title"`             // Movie title
+	Year      int32     `json:"year,omitempty"`    // Movie release year
+	Runtime   int32     `json:"runtime,omitempty"` // Movie runtime (in minutes)
+	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie
+	Version   int32     `json:"veersion"`          // The version number starts at 1 and will be incremented each time the movie information is updated
+}
