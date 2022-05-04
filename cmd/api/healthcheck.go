5ded19e42b1f8df903e90cd8379c3c7c266298b1
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-04 22:52:08 +0400

    json encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // Handler for writing application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a fixed-format JSON response from a string.
-	json := `{"status": "available", "environment": %q, "version": %q}`
-	json = fmt.Sprintf(json, app.config.env, version)
+	// json := `{"status": "available", "environment": %q, "version": %q}`
+	// json = fmt.Sprintf(json, app.config.env, version)
+
+	// Create a map which holds the information
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 
 	// Set the "Content-Type: application/json"
 	w.Header().Set("Content-Type", "application/json")
 
-	// Write the JSON as the HTTP response body.
-	w.Write([]byte(json))
+	// Send the []byte slice containing the JSON as the response body.
+	w.Write(jsonData)
 }
