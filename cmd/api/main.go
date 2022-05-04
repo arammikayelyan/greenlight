16430d3c25e6a2ff95e7587b05a94421e3a3bdc3
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-04 17:37:13 +0400

    run the server on the healthcheck route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declare the application version number (later it'll be generated automatically)
+const version = "1.0.0"
+
+// Application configuration settings
+type config struct {
+	port int
+	env  string
+}
+
+// Application struct to hold the dependencies
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Declare an instance of the config struct
+	var cfg config
+
+	// Read the value of the port and env command-line flags into the config.
+	// Default port number 4000 and the environment "development"
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new logger which writes messages to the standard out stream,
+	// prefixed with the current date and time.
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// An instance of application struct
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// ServeMux handler for the server
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start the HTTP server
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
