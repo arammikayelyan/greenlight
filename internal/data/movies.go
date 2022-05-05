ec57bad67fbd01019ce19bfec778d445ae5e651a
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-05 21:01:28 +0400

    delete method and handler

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -84,6 +84,26 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM movies
+		WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffeted, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffeted == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
 
