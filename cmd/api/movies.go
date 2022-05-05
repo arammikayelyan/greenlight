f9064f83f3d54f53b7f45a15f56ea92d28843a5d
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-05 21:27:39 +0400

    change the fields of input struct to pointers so we can update them partially

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -100,10 +100,10 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	// Declare an input struct to hold the expected data from the client.
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	// 3. Read the JSON request body
@@ -114,10 +114,18 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	// 4. Copy the data across from the input struct to the movie record
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	// 5. Check that the updated movie record is valid using the data.VAlidateMovie()
 	// function
