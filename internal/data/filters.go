fc278a4185012824d1961ed66e53c5eb9e6e0b30
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-06 17:52:57 +0400

    calculate and return pagination metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"greenlight.arammikayelyan.dev/internal/validator"
@@ -11,6 +12,33 @@ type Filters struct {
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+// calculateMetdata calculates the appropriate pagination metadata values given
+// the total number of records, current page, and page size values. The last
+// page value is calculated using the math.Ceil() function, which rounds up a
+// float to the nearest integer. So, for example, if there were 12 records in
+// total and a page size of 5, the last page value would be math.Ceil(12/5) = 3.
+func calculateMetdata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
 
 // sortColumn checks the client-provided Sort field matches one of the entries
