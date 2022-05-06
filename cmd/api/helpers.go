998b7ad033daf2aead6ad34ec45b2d316e10d24b
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-06 11:08:30 +0400

    listMoviesHandler with readString, readInt and readCSV helpers for parsing a query string

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"greenlight.arammikayelyan.dev/internal/validator"
 )
 
 // Define an envelope type.
@@ -105,4 +107,42 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 
 	return nil
+}
+
+// readString helper returns a string value from the query string, or the provided
+// default value if no matching key could be found.
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+// readCSV helper returns a string value from the query string, and then splits
+// it into a slice on the comma character. If no matching could be found, it
+// returns the provided default value.
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+	return strings.Split(csv, ",")
+}
+
+// readInt helper reads a string value from the query string and converts it to
+// an integer before returning. If no matching key could be found it returns the
+// provided default value. If the value couldn't be converted to an integer, then
+// we record an error message in the provided validator instance.
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+	return i
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,10 +14,11 @@ func (app *application) routes() *httprouter.Router {
 	// http.HandlerFunc() adapter, and then set it as the custom error handler
 	// for 404 Not Found responses.
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovieHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
