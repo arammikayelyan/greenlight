056fe82b2b74a2e1c5e7cacd7efec726c35ab4ef
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2022-05-09 16:27:52 +0400

    IP-based rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -20,14 +23,58 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Average of 2 requests per second, with a maximum of 4 requests in single 'burst'
-	limiter := rate.NewLimiter(2, 4)
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a background goroutine which removes old entries from the clients
+	// map once every minute
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		// Extract the client's IP addr from the request
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// Lock the mutext to prevent this code being executed concurrently.
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			// Create and add a new client if does not already exist
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
